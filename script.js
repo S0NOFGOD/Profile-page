9bfdc2fc0120edf5f4532fe56f4b93d96f36f92e
Destiny Okpone <destiny.okpone@gmail.com>
2025-12-31 17:13:14 +0100

    updated my index file

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-const form = document.getElementById("contactForm");
-const response = document.getElementById("response");
-
-form.addEventListener("submit", function(e) {
-  e.preventDefault(); // Prevent page reload
-  const name = document.getElementById("name").value;
-  response.innerText = `Thank you, ${name}! Your message has been received.`;
-  form.reset();
+document.querySelectorAll(".social-links a").forEach(link => {
+    link.addEventListener("click", function(){
+        alert("You are visiting " + link.textContent);
+    });
 });
